073fff2e20b34efa47f38590f1e9715b66eeebd8
Doxygen CI <travis@travis-ci.org>
2020-12-06 18:57:36 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['getbaseline',['getBaseline',['../class_adafruit___c_c_s811.html#ad15ce417ac4fe460ecb3ff86e6a1641d',1,'Adafruit_CCS811']]],
+  ['getcurrentselected',['getCurrentSelected',['../class_adafruit___c_c_s811.html#a36d5260503d660e0caf6ccad192acd59',1,'Adafruit_CCS811']]],
   ['geteco2',['geteCO2',['../class_adafruit___c_c_s811.html#a6a07e274e92c1c498c1855a6ec8ea7a4',1,'Adafruit_CCS811']]],
+  ['getrawadcreading',['getRawADCreading',['../class_adafruit___c_c_s811.html#adbe4676982ed7cb5be9566ec73618373',1,'Adafruit_CCS811']]],
   ['gettvoc',['getTVOC',['../class_adafruit___c_c_s811.html#a928028d135e5b69d7a90fcff1e8895a0',1,'Adafruit_CCS811']]]
 ];
