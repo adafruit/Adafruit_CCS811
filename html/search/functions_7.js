ae6b66285c55e11581e92e3cc55459e040d7076c
Doxygen CI <travis@travis-ci.org>
2020-12-07 19:55:54 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_7.js
+++ html/search/functions_7.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['setbaseline',['setBaseline',['../class_adafruit___c_c_s811.html#a9070823b9b5fe462ab20333f3bddd7d1',1,'Adafruit_CCS811']]],
   ['setdrivemode',['setDriveMode',['../class_adafruit___c_c_s811.html#a3e6018dfec23763bf48344e81a72675a',1,'Adafruit_CCS811']]],
-  ['setenvironmentaldata',['setEnvironmentalData',['../class_adafruit___c_c_s811.html#a3988881051226395e4858d9515a126a7',1,'Adafruit_CCS811']]],
+  ['setenvironmentaldata',['setEnvironmentalData',['../class_adafruit___c_c_s811.html#a39e6cd8f7a267c04a743f6d2d2fc1978',1,'Adafruit_CCS811']]],
   ['settempoffset',['setTempOffset',['../class_adafruit___c_c_s811.html#a71df73bd3538f64ecc3fee7ee41c53d6',1,'Adafruit_CCS811']]],
   ['setthresholds',['setThresholds',['../class_adafruit___c_c_s811.html#a388c8751568f90cd8369766deb23fbb8',1,'Adafruit_CCS811']]],
   ['swreset',['SWReset',['../class_adafruit___c_c_s811.html#abcf5367bc8a36000228d2eabb2a15666',1,'Adafruit_CCS811']]]
