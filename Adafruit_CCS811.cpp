46afc07747d7c0a0e8c89f76186a2801e1c543d7
caternuson <caternuson@gmail.com>
2021-08-11 08:41:55 -0700

    remove chunk

--- Adafruit_CCS811.cpp
+++ Adafruit_CCS811.cpp
@@ -273,29 +273,8 @@ uint8_t Adafruit_CCS811::read8(byte reg) {
 }
 
 void Adafruit_CCS811::read(uint8_t reg, uint8_t *buf, uint8_t num) {
-  uint8_t buffer[1];
-  size_t chunkSize = i2c_dev->maxBufferSize();
-  uint8_t pos = 0;
-
-  if (chunkSize > num) {
-    // can just read
-    buffer[0] = reg;
-    i2c_dev->write(buffer, 1);
-    i2c_dev->read(buf, num);
-  } else {
-    // must read in chunks
-    uint8_t read_buffer[chunkSize];
-    while (pos < num) {
-      buffer[0] = reg + pos;
-      i2c_dev->write(buffer, 1);
-      uint8_t read_now = min(uint8_t(chunkSize), (uint8_t)(num - pos));
-      i2c_dev->read(read_buffer, read_now);
-      for (uint8_t i = 0; i < read_now; i++) {
-        buf[pos] = read_buffer[i];
-        pos++;
-      }
-    }
-  }
+  uint8_t buffer[1] = {reg};
+  i2c_dev->write_then_read(buffer, 1, buf, num);
 }
 
 void Adafruit_CCS811::write(uint8_t reg, uint8_t *buf, uint8_t num) {
